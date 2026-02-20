8250265910ffaac04fa4efcaa9bc4fe6d2de47bf
Niels Mouthaan <nielsmouthaan@me.com>
2026-02-20 10:49:25 +0100

    Remove unnecessary availability guard for encrypted key

--- ejectify/Model/ExternalVolume.swift
+++ ejectify/Model/ExternalVolume.swift
@@ -135,12 +135,7 @@ class ExternalVolume {
             return nil
         }
 
-        let encrypted: Bool
-        if #available(macOS 10.14.4, *) {
-            encrypted = diskInfo[kDADiskDescriptionMediaEncryptedKey] as? Bool ?? false
-        } else {
-            encrypted = false
-        }
+        let encrypted = diskInfo[kDADiskDescriptionMediaEncryptedKey] as? Bool ?? false
 
         return ExternalVolume(disk: disk, id: id as String, name: name, bsdName: bsdName, encrypted: encrypted)
     }
